3c0c4f274bd1f0c8441533e0edc118b5130a93b1
nathan130200 <nathan130200@gmail.com>
2020-02-25 16:43:49 -0300

    Reajustado métodos para o fim do arquivo.

--- Lana/Modules/MusicModule.cs
+++ Lana/Modules/MusicModule.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#pragma warning disable CS1998 
+
+using System;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Reflection;
@@ -18,18 +20,53 @@ namespace Lana.Modules
         private LavalinkNodeConnection node;
         private LavalinkGuildConnection connection;
         private ConcurrentQueue<TrackInfo> tracks;
-        private TrackInfo currentTrack;
+        private TrackInfo state;
         private LavalinkConfiguration config;
         private LanaBot bot;
 
         public MusicModule(LanaBot bot)
         {
             this.bot = bot;
-            this.bot.Lavalink.NodeDisconnected += this.ProcessNodeDisconnected;
-            this.config = new LavalinkConfiguration(this.bot.Configuration.Lavalink.Build());
+            this.tracks = new ConcurrentQueue<TrackInfo>();
+            this.state = default;
+            this.bot.Lavalink.NodeDisconnected += this.NotifyNodeDisconnected;
+            this.UpdateConfiguration();
         }
 
-        Task ProcessNodeDisconnected(NodeDisconnectedEventArgs e)
+        public void UpdateConfiguration()
+            => this.config = new LavalinkConfiguration(this.bot.Configuration.Lavalink.Build());
+
+        [Command, RequireVoiceChannel, RequireSameVoiceChannel, Priority(0)]
+        public async Task Play(CommandContext ctx, string search)
+        {
+
+        }
+
+        [Command, RequireVoiceChannel, RequireSameVoiceChannel, Priority(1)]
+        public async Task Play(CommandContext ctx, Uri url)
+        {
+            var load_result = await this.node.Rest.GetTracksAsync(uri);
+        }
+
+        async Task<int> PlayTrackAsync(TrackInfo track)
+        {
+            if (this.connection == null)
+                return -1;
+
+            if (this.state != null)
+            {
+                this.tracks.Enqueue(track);
+                return 1;
+            }
+            else
+            {
+                this.state = track;
+                await this.connection.PlayAsync(this.state.Track);
+                return 0;
+            }
+        }
+
+        Task NotifyNodeDisconnected(NodeDisconnectedEventArgs e)
         {
             if (e.LavalinkNode == this.node)
                 this.connection = default;
@@ -39,6 +76,8 @@ namespace Lana.Modules
 
         public override async Task BeforeExecutionAsync(CommandContext ctx)
         {
+            this.UpdateConfiguration();
+
             var lavalink = ctx.Client.GetLavalink();
 
             if (this.node == null || !this.node.IsConnected)
@@ -59,7 +98,7 @@ namespace Lana.Modules
             else
                 this.connection = await this.node.ConnectAsync(memberVoiceChannel);
 
-            if(this.connection != null && !this.connection.IsConnected)
+            if (this.connection != null && !this.connection.IsConnected)
             {
                 var objRef = this.node.GetType().GetProperty("ConnectedGuilds",
                     BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this.node);
@@ -69,18 +108,6 @@ namespace Lana.Modules
 
                 this.connection = await this.node.ConnectAsync(memberVoiceChannel);
             }
-        }
-
-        [Command, RequireVoiceChannel, RequireSameVoiceChannel, Priority(0)]
-        public async Task Play(CommandContext ctx, string search)
-        {
-
-        }
-
-        [Command, RequireVoiceChannel, RequireSameVoiceChannel, Priority(1)]
-        public async Task Play(CommandContext ctx, Uri url)
-        {
-            
         }
     }
 }
