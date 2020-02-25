2f016d4764f5953de0e454f3a57b76df42964f05
nathan130200 <nathan130200@gmail.com>
2020-02-25 13:19:56 -0300

    Commit inicial.

---
+++ Lana/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
