ecd68973dd61c605fb414d21e3bfc10b9b50f357
nathan130200 <nathan130200@gmail.com>
2020-02-29 21:24:01 -0300

    Movido comando de ping para módulo de informações.

--- Lana/Modules/AdministrationModule.cs
+++ Lana/Modules/AdministrationModule.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
 using Lana.Attributes;
 
 namespace Lana.Modules
@@ -27,21 +24,6 @@ namespace Lana.Modules
 			var success = await ctx.RespondAsync(":white_check_mark: As mensagens foram deletadas com sucesso.\n*Esta mensagem irá se auto-destruir em cinco segundos.*");
 			await Task.Delay(5000);
 			await success.DeleteAsync();
-		}
-
-		[Command("ping")]
-		public async Task PingAsync(CommandContext ctx)
-		{
-			var watch = Stopwatch.StartNew();
-			await ctx.TriggerTypingAsync();
-			watch.Stop();
-			
-			await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
-				.WithAuthor("Ping", iconUrl: ctx.Client.CurrentUser.AvatarUrl)
-				.AddField(":zap: Rest", $"{watch.ElapsedMilliseconds}ms", true)
-				.AddField(":satellite_orbital: Gateway", $"{ctx.Client.Ping}ms", true)
-				.WithColor(DiscordColor.Blurple)
-				.WithTimestamp(DateTime.Now));
 		}
 	}
 }
---
+++ Lana/Modules/InformationModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace Lana.Modules
+{
+	public class InformationModule : BaseCommandModule
+	{
+		[Command("ping")]
+		public async Task PingAsync(CommandContext ctx)
+		{
+			var watch = Stopwatch.StartNew();
+			await ctx.TriggerTypingAsync();
+			watch.Stop();
+
+			await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+				.WithAuthor("Ping", iconUrl: ctx.Client.CurrentUser.AvatarUrl)
+				.AddField(":zap: Rest", $"{watch.ElapsedMilliseconds}ms", true)
+				.AddField(":satellite_orbital: Gateway", $"{ctx.Client.Ping}ms", true)
+				.WithColor(DiscordColor.Blurple)
+				.WithTimestamp(DateTime.Now));
+		}
+	}
+}
